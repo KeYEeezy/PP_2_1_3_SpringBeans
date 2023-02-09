321fe24c125714808515aaba23f6a7331de3d538
KeYEeezy <112856337+KeYEeezy@users.noreply.github.com>
2023-02-09 11:38:47 +0300

    Done!

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,17 @@
+package app.config;
+
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Time;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+
+}
